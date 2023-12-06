39f21a502ef93924c3851589d4da51e04e528934
Carlos Alejo <Carlos.Alejo@bestsecret.com>
2023-12-06 13:12:56 +0100

    Code refactored for day 6

--- D06.cs
+++ D06.cs
@@ -4,35 +4,24 @@
     {
         internal override void SolvePart1()
         {
-            var ts = new int[] { 54, 70, 82, 75 };
-            var ds = new int[] { 239, 1142, 1295, 1253 };
-
-            long p = 1;
-            for (int i = 0; i < ts.Length; i++)
-            {
-                long c = 0;
-                for (int v = 0; v < ts[i]; v++)
-                {
-                    if (v * (ts[i] - v) > ds[i]) c++;
-                }
-                p *= c;
-            }
-
-            Console.WriteLine(p);
+            var rs = new (long, long)[] { (54, 239), (70, 1142), (82, 1295), (75, 1253) };
+            Console.WriteLine(rs.Select(r => C(r)).Product());
         }
 
         internal override void SolvePart2()
         {
-            long t = 54708275;
-            long d = 239114212951253;
+            (long, long) r = (54708275, 239114212951253);
+            Console.WriteLine(C(r));
+        }
 
-            long c = 0;
-            for (int v = 0; v < t; v++)
+        private static long C((long T, long D) r)
+        {
+            long v = 0;
+            for (; v < r.T; v++)
             {
-                if (v * (t - v) > d) c++;
+                if (v * (r.T - v) > r.D) break;
             }
-
-            Console.WriteLine(c);
+            return r.T + 1 - v * 2;
         }
     }
 }
--- Extensions.cs
+++ Extensions.cs
@@ -2,28 +2,17 @@
 {
     internal static class Extensions
     {
-        internal static IEnumerable<IEnumerable<T>> GetPermutations<T>(this IEnumerable<T> list, int length)
-        {
-            if (length == 1)
-                return list.Select(t => new T[] { t });
+        internal static IEnumerable<IEnumerable<T>> GetPermutations<T>(this IEnumerable<T> list, int length) =>
+            length == 1
+                ? list.Select(t => new T[] { t })
+                : GetPermutations(list, length - 1)
+                    .SelectMany(t => list.Where(e => !t.Contains(e)),
+                        (t1, t2) => t1.Concat(new T[] { t2 }));
 
-            return GetPermutations(list, length - 1)
-                .SelectMany(t => list.Where(e => !t.Contains(e)),
-                    (t1, t2) => t1.Concat(new T[] { t2 }));
-        }
+        internal static long Product(this IEnumerable<long> list) =>
+            list.Aggregate(1L, (current, item) => current * item);
 
-        internal static long Product(this IEnumerable<long> list)
-        {
-            long r = 1;
-
-            foreach (var item in list) { r *= item; }
-
-            return r;
-        }
-
-        internal static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> source)
-        {
-            return source.Select((item, index) => (item, index));
-        }
+        internal static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> source) =>
+            source.Select((item, index) => (item, index));
     }
 }
