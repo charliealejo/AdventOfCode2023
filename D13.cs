b5883887013ab9210909d40a80517750151d8451
Carlos Alejo <Carlos.Alejo@bestsecret.com>
2023-12-15 17:01:15 +0100

    Added days 13 and 14

---
+++ D13.cs
@@ -0,0 +1,107 @@
+﻿namespace AdventOfCode2023
+{
+    internal class D13 : Day
+    {
+        internal override void SolvePart1()
+        {
+            var lines = FileHelper.ReadLines(FileName);
+            var maps = CreateMaps(lines);
+
+            long res = 0;
+
+            foreach (var map in maps)
+            {
+                res += CalculateSymmetry(map);
+            }
+
+            Console.WriteLine(res);
+        }
+
+        internal override void SolvePart2()
+        {
+            var lines = FileHelper.ReadLines(FileName);
+            var maps = CreateMaps(lines);
+
+            long res = 0;
+
+            foreach (var map in maps)
+            {
+                var score = CalculateSymmetry(map);
+                var found = false;
+                for (int i = 0; i < map.Length; i++)
+                {
+                    for (int j = 0; j < map[i].Length; j++)
+                    {
+                        var c = map[i][j];
+                        map[i] = map[i][..j] + (map[i][j] == '.' ? '#' : '.') + map[i][(j + 1)..];
+                        var s = CalculateSymmetry(map, score);
+                        if (s != 0 && s != score)
+                        {
+                            res += s;
+                            found = true;
+                            break;
+                        }
+                        map[i] = map[i][..j] + c + map[i][(j + 1)..];
+                    }
+                    if (found) break;
+                }
+                if (!found) { throw new Exception(); }
+            }
+
+            Console.WriteLine(res);
+        }
+
+        private static string[][] CreateMaps(IEnumerable<string> lines)
+        {
+            var maps = new List<string[]>();
+            var map = new List<string>();
+            foreach (var l in lines)
+            {
+                if (string.IsNullOrEmpty(l))
+                {
+                    maps.Add(map.ToArray());
+                    map = new List<string>();
+                }
+                else
+                {
+                    map.Add(l);
+                }
+            }
+            maps.Add(map.ToArray());
+            return maps.ToArray();
+        }
+
+        private static long CalculateSymmetry(string[] map, long prev = 0)
+        {
+            for (int i = 1; i < map.Length; i++)
+            {
+                var h = map[..i];
+                var t = map[i..];
+                if (h.Length > t.Length) h = h.Skip(h.Length - t.Length).ToArray();
+                else t = t.Take(h.Length).ToArray();
+                h = h.Reverse().ToArray();
+
+                if (h.WithIndex().All(l => l.item == t[l.index]) && (100 * i != prev)) return 100 * i;
+            }
+
+            var mapp = new string[map[0].Length];
+            foreach (var (_, i) in map[0].WithIndex())
+            {
+                mapp[i] = new string(map.Select(l => l[i]).ToArray());
+            }
+
+            for (int i = 1; i < mapp.Length; i++)
+            {
+                var h = mapp[..i];
+                var t = mapp[i..];
+                if (h.Length > t.Length) h = h.Skip(h.Length - t.Length).ToArray();
+                else t = t.Take(h.Length).ToArray();
+                h = h.Reverse().ToArray();
+
+                if (h.WithIndex().All(l => l.item == t[l.index]) && i != prev) return i;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ D14.cs
@@ -0,0 +1,180 @@
+﻿using System.Text;
+
+namespace AdventOfCode2023
+{
+    internal class D14 : Day
+    {
+        internal override void SolvePart1()
+        {
+            var map = FileHelper.ReadLines(FileName).Select(l => l.ToCharArray()).ToArray();
+            map.TiltNorth();
+            Console.WriteLine(map.WeightOnNorth());
+        }
+
+        internal override void SolvePart2()
+        {
+            var map = FileHelper.ReadLines(FileName).Select(l => l.ToCharArray()).ToArray();
+            var d = new Dictionary<string, int>();
+            var l = 0;
+            for (int i = 0; i < 1000000000; i++)
+            {
+                map.TiltNorth();
+                map.TiltWest();
+                map.TiltSouth();
+                map.TiltEast();
+
+                if (l == 0)
+                {
+                    var w = map.GetKey();
+                    if (d.ContainsKey(w))
+                    {
+                        l = i - d[w];
+                        var s = (1000000000 - i) % l;
+                        i = 1000000000 - s;
+                    }
+                    else
+                    {
+                        d.Add(w, i);
+                    }
+                }
+            }
+            Console.WriteLine(map.WeightOnNorth());
+        }
+    }
+
+    internal static class E
+    {
+        internal static void TiltNorth(this char[][] map)
+        {
+            for (var i = 1; i < map.Length; i++)
+            {
+                for (var j = 0; j < map[i].Length; j++)
+                {
+                    if (map[i][j] == 'O')
+                    {
+                        map.PushNorth(i, j);
+                    }
+                }
+            }
+        }
+
+        internal static void PushNorth(this char[][] map, int i, int j)
+        {
+            if (i == 0) return;
+            if (map[i - 1][j] == '.')
+            {
+                map[i - 1][j] = 'O';
+                map[i][j] = '.';
+                map.PushNorth(i - 1, j);
+            }
+        }
+
+        internal static void TiltSouth(this char[][] map)
+        {
+            for (var i = map.Length - 1; i >= 0; i--)
+            {
+                for (var j = 0; j < map[i].Length; j++)
+                {
+                    if (map[i][j] == 'O')
+                    {
+                        map.PushSouth(i, j);
+                    }
+                }
+            }
+        }
+
+        internal static void PushSouth(this char[][] map, int i, int j)
+        {
+            if (i == map.Length - 1) return;
+            if (map[i + 1][j] == '.')
+            {
+                map[i + 1][j] = 'O';
+                map[i][j] = '.';
+                map.PushSouth(i + 1, j);
+            }
+        }
+
+        internal static void TiltWest(this char[][] map)
+        {
+            for (var i = 0; i < map.Length; i++)
+            {
+                for (var j = 1; j < map[i].Length; j++)
+                {
+                    if (map[i][j] == 'O')
+                    {
+                        map.PushWest(i, j);
+                    }
+                }
+            }
+        }
+
+        internal static void PushWest(this char[][] map, int i, int j)
+        {
+            if (j == 0) return;
+            if (map[i][j - 1] == '.')
+            {
+                map[i][j - 1] = 'O';
+                map[i][j] = '.';
+                map.PushWest(i, j - 1);
+            }
+        }
+
+        internal static void TiltEast(this char[][] map)
+        {
+            for (var i = 0; i < map.Length; i++)
+            {
+                for (var j = map[i].Length - 1; j >= 0; j--)
+                {
+                    if (map[i][j] == 'O')
+                    {
+                        map.PushEast(i, j);
+                    }
+                }
+            }
+        }
+
+        internal static void PushEast(this char[][] map, int i, int j)
+        {
+            if (j == map[i].Length - 1) return;
+            if (map[i][j + 1] == '.')
+            {
+                map[i][j + 1] = 'O';
+                map[i][j] = '.';
+                map.PushEast(i, j + 1);
+            }
+        }
+
+        internal static long WeightOnNorth(this char[][] map)
+        {
+            long res = 0;
+
+            for (var i = 0; i < map.Length; i++)
+            {
+                for (var j = 0; j < map[i].Length; j++)
+                {
+                    if (map[i][j] == 'O')
+                    {
+                        res += map.Length - i;
+                    }
+                }
+            }
+
+            return res;
+        }
+
+        internal static string GetKey(this char[][] map)
+        {
+            var key = new StringBuilder();
+
+            for (var i = 0; i < map.Length; i++)
+            {
+                for (var j = 0; j < map[i].Length; j++)
+                {
+                    key.Append(map[i][j]);
+                }
+            }
+
+            return key.ToString();
+        }
+    }
+}
