64ffc59af65cb8da6f3a43a48dd862682eb981d2
Carlos Alejo <Carlos.Alejo@bestsecret.com>
2024-01-10 11:37:40 +0100

    Added day 24 part 2

--- D24.cs
+++ D24.cs
@@ -11,7 +11,11 @@
 
         internal override void SolvePart2()
         {
+            var lines = FileHelper.ReadLines(FileName);
+            var stones = GetStones(lines);
+            long total = Solve(stones);
 
+            Console.WriteLine(total);
         }
 
         private static Stone[] GetStones(IEnumerable<string> lines)
@@ -71,6 +75,99 @@
             }
 
             return intersections;
+        }
+
+        private static long Solve(Stone[] st)
+        {
+            int i, j, k, imax;
+            double temp, f, maxi;
+            long total;
+
+            double[,] a = new double[6, 6];
+            a[0, 0] = st[1].Vy - st[2].Vy;
+            a[0, 1] = st[2].Vx - st[1].Vx;
+            a[0, 3] = st[2].Y - st[1].Y;
+            a[0, 4] = st[1].X - st[2].X;
+            a[1, 0] = st[1].Vy - st[3].Vy;
+            a[1, 1] = st[3].Vx - st[1].Vx;
+            a[1, 3] = st[3].Y - st[1].Y;
+            a[1, 4] = st[1].X - st[3].X;
+            a[2, 0] = st[1].Vz - st[2].Vz;
+            a[2, 2] = st[2].Vx - st[1].Vx;
+            a[2, 3] = st[2].Z - st[1].Z;
+            a[2, 5] = st[1].X - st[2].X;
+            a[3, 0] = st[1].Vz - st[3].Vz;
+            a[3, 2] = st[3].Vx - st[1].Vx;
+            a[3, 3] = st[3].Z - st[1].Z;
+            a[3, 5] = st[1].X - st[3].X;
+            a[4, 1] = st[1].Vz - st[2].Vz;
+            a[4, 2] = st[2].Vy - st[1].Vy;
+            a[4, 4] = st[2].Z - st[1].Z;
+            a[4, 5] = st[1].Y - st[2].Y;
+            a[5, 1] = st[1].Vz - st[3].Vz;
+            a[5, 2] = st[3].Vy - st[1].Vy;
+            a[5, 4] = st[3].Z - st[1].Z;
+            a[5, 5] = st[1].Y - st[3].Y;
+
+            double[] c = new double[6];
+            c[0] = (st[2].Y * st[2].Vx - st[2].X * st[2].Vy) - (st[1].Y * st[1].Vx - st[1].X * st[1].Vy);
+            c[1] = (st[3].Y * st[3].Vx - st[3].X * st[3].Vy) - (st[1].Y * st[1].Vx - st[1].X * st[1].Vy);
+            c[2] = (st[2].Z * st[2].Vx - st[2].X * st[2].Vz) - (st[1].Z * st[1].Vx - st[1].X * st[1].Vz);
+            c[3] = (st[3].Z * st[3].Vx - st[3].X * st[3].Vz) - (st[1].Z * st[1].Vx - st[1].X * st[1].Vz);
+            c[4] = (st[2].Z * st[2].Vy - st[2].Y * st[2].Vz) - (st[1].Z * st[1].Vy - st[1].Y * st[1].Vz);
+            c[5] = (st[3].Z * st[3].Vy - st[3].Y * st[3].Vz) - (st[1].Z * st[1].Vy - st[1].Y * st[1].Vz);
+
+            for (k = 0; k < a.GetLength(0) - 1; k++)
+            {
+                imax = 0;
+                maxi = 0.0;
+
+                for (i = k; i < a.GetLength(0); i++)
+                {
+                    temp = Math.Abs(a[i, k]);
+                    if (temp > maxi)
+                    {
+                        maxi = temp;
+                        imax = i;
+                    }
+                }
+
+                temp = c[k];
+                c[k] = c[imax];
+                c[imax] = temp;
+
+                for (i = 0; i < a.GetLength(0); i++)
+                {
+                    temp = a[k, i];
+                    a[k, i] = a[imax, i];
+                    a[imax, i] = temp;
+                }
+
+                for (i = k + 1; i < c.Length; i++)
+                {
+                    f = a[i, k] / a[k, k];
+                    for (j = k; j < a.GetLength(0); j++)
+                    {
+                        a[i, j] -= a[k, j] * f;
+                    }
+                    c[i] -= c[k] * f;
+                }
+            }
+
+            double[] x = new double[6];
+
+            for (k = a.GetLength(0) - 1; k >= 0; k--)
+            {
+                x[k] = c[k];
+                for (i = k + 1; i < a.GetLength(0); i++)
+                {
+                    x[k] -= a[k, i] * x[i];
+                }
+                x[k] /= a[k, k];
+            }
+
+            total = (long)Math.Round(x[0]) + (long)Math.Round(x[1]) + (long)Math.Round(x[2]);
+            return total;
         }
 
         internal class Stone
